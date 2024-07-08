567c8a1d514e9a3a605194a4558f1cd6ba50aaef
Shiv Vyas <shivvyas0209@gmail.com>
2024-07-08 17:08:53 -0400

    Content View Updated

--- ExpenseTracker/ContentView.swift
+++ ExpenseTracker/ContentView.swift
@@ -8,11 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var transactionListVM: TransactionListViewModel
     var body: some View {
         NavigationView {
             ScrollView{
                 VStack(alignment: .leading, spacing: 20){
                     Text("Overview").font(.title).bold()
+                    RecentTransactionList()
                 }.padding().frame(maxWidth: .infinity)
                 
                 
@@ -24,6 +26,18 @@ struct ContentView: View {
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static let transactionListVM: TransactionListViewModel = {
+        let transactionListVM = TransactionListViewModel()
+        transactionListVM.transcations = transactionListPreviewData
+        return transactionListVM
+    }()
+    static var previews: some View{
+        Group {
+            ContentView()
+            ContentView().preferredColorScheme(.dark)
+        }
+        .environmentObject(transactionListVM)
+    }
+
 }
--- ExpenseTracker/ExpenseTrackerApp.swift
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -14,6 +14,7 @@ struct ExpenseTrackerApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView().environmentObject(transcationListVM)
+            
         }
     }
 }
